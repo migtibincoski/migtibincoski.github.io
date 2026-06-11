dc9a02e25de7f1abf5c06230e9cb56a1b277f75f
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2026-06-10 22:36:05 -0300

    added copy text page

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Button } from "./components/ui/button.tsx";
-import { ArrowUpRightIcon, HomeIcon } from "lucide-react";
+import { ArrowUpRightIcon, CopyIcon, HomeIcon, Undo2Icon } from "lucide-react";
+import { Textarea } from "./components/ui/textarea.tsx";
 
 const url = new URL(window.location.href);
 const redirect = url.searchParams.get("auto-redirect-to");
+const copyText = url.searchParams.get("copy-text");
+console.debug(url, redirect, copyText);
+
 if (redirect && String(redirect).trim() !== "") {
   const urls = {
     homepage: "https://migtibincoski.is-a.dev/",
@@ -59,6 +63,38 @@ if (redirect && String(redirect).trim() !== "") {
       </div>,
     );
   }
+} else if (copyText && String(copyText).trim() !== "") {
+  navigator.clipboard.writeText(copyText).then(() => {
+    alert("Text copiado com sucesso!");
+    window.history.back();
+  });
+
+  createRoot(document.getElementById("root")!).render(
+    <div className="h-screen w-screen flex flex-col gap-2.5 justify-center items-center">
+      <h1 className="max-w-md text-center">Copie o texto abaixo:</h1>
+      <Textarea className="max-w-sm w-[75vw]" value={copyText} disabled />
+      <div className="flex flex-row gap-2.5">
+        <Button
+          variant="default"
+          className="bg-gradient-primary hover:shadow-glow transition-all duration-300"
+          onClick={() =>
+            navigator.clipboard.writeText(copyText).then(() => {
+              alert("Text copiado com sucesso!");
+              window.history.back();
+              window.close();
+            })
+          }
+        >
+          Copiar texto
+          <CopyIcon />
+        </Button>
+        <Button variant="secondary" onClick={() => window.history.back()}>
+          Voltar
+          <Undo2Icon />
+        </Button>
+      </div>
+    </div>,
+  );
 } else {
   createRoot(document.getElementById("root")!).render(<App />);
 }
